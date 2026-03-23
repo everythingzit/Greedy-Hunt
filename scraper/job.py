70a241108f725cc92dadf2cf16a629c860323f30
Hendrik Tebeng <hnt.nzodoum@gmail.com>
2026-03-23 13:11:47 -0400

    Create Job class fields.

--- scraper/job.py
+++ scraper/job.py
@@ -1,3 +1,9 @@
 class Job:
-    def __init__(self):
-        pass
+    def __init__(self, title, company, locations, url, date_posted):
+        self.title = title
+        self.company = company
+        self.locations = locations
+        self.url = url
+        self.date_posted = date_posted
+
+        
