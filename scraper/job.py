3c481582c52fd748fe6af584c8f7784dbc6d979e
Hendrik Tebeng <hnt.nzodoum@gmail.com>
2026-03-23 13:44:04 -0400

    Adjust location requirement.

--- scraper/job.py
+++ scraper/job.py
@@ -1,9 +1,8 @@
 class Job:
-    def __init__(self, title, company, locations, url, date_posted):
+    def __init__(self, title, company, location, url, date_posted):
         self.title = title
         self.company = company
-        self.locations = locations
+        self.location = location
         self.url = url
         self.date_posted = date_posted
 
-        
